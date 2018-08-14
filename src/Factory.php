4183d6c59fe5a140216865ac2fd27955538b58cf
Mohamed Kamel <Mohamed.Kamel@student.aast.edu>
2018-08-14 05:33:22 +0200

    Model factory

--- src/Factory.php
+++ src/Factory.php
@@ -1,9 +1,49 @@
-<?php
+<?php namespace Src;
 
-namespace Src;
+use Src\Models\Coach;
+use Src\Models\Student;
+use Src\Models\CoachStudent;
 
-
+/**
+ * Class Factory
+ *
+ * @package Src
+ */
 class Factory
 {
+    /**
+     * @param $table
+     * @param $count
+     * @return bool
+     */
+    public static function create($table, $count)
+    {
+        if(is_null(self::getInstanceOfModel($table))){
+           return false;
+        }
 
+        $model = self::getInstanceOfModel($table);
+        for ($i = 0; $i < $count; $i++) {
+            $model->name = "test_name";
+            $model->save();
+        }
+    }
+
+    /**
+     * @param $table
+     * @return \Src\Models\Coach|\Src\Models\CoachStudent|\Src\Models\Student
+     */
+    private static function getInstanceOfModel($table)
+    {
+        switch ($table) {
+            case "students":
+                return new Student();
+                break;
+            case "coaches":
+                return new Coach();
+                break;
+            default:
+                return null;
+        }
+    }
 }
